02a4ff6d2bc0934885add8ab66bbd88b47833ba7
Ridwan226 <4515210063@univpancasila.ac.id>
2021-02-10 00:32:55 +0700

    setting Midelware

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 require("dotenv/config");
 
 const api = process.env.API_URL;
 
+// Midelware
+app.use(bodyParser.json());
+
 // http://localhost:3000//api/v1/product
 
-app.get(api + "/product", (req, res) => {
-  res.send("Hello API");
+app.get(`${api}/product`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "Baju",
+    image: "default.png",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/product`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 
 app.listen(3000, () => {
