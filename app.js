3d72b6913cd390b5b73508b7fd3e4e37bc0d6151
Ridwan226 <4515210063@univpancasila.ac.id>
2021-02-10 01:13:04 +0700

    Created Product and get

--- app.js
+++ app.js
@@ -13,19 +13,51 @@ app.use(morgan("tiny"));
 
 // http://localhost:3000//api/v1/product
 
-app.get(`${api}/product`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "Baju",
-    image: "default.png",
-  };
-  res.send(product);
+const productSchema = mongoose.Schema({
+  name: String, // String is shorthand for {type: String}
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.get(`${api}/product`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({
+      success: false,
+    });
+  }
+
+  res.send(productList);
 });
 
 app.post(`${api}/product`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.send(newProduct);
+  // const newProduct = req.body;
+  // console.log(newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+
+  // res.send(newProduct);
 });
 
 mongoose
